b84960a27cf82cee41bc516a9475a78e5e34fc78
Alex Simenduev <shamil@boxee.tv>
2013-04-16 15:15:03 +0000

    refactor everything :), ditch node-imageable

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 // requires
 var path        = require('fs'),
+    log         = require('sslog'),
     config      = require('../lib/config');
 
 // get the NODE_ENV
@@ -7,18 +8,20 @@ var env = process.env.NODE_ENV;
 
 // load default config options
 config.load('./config/default.json', function(err) {
-    if (err) {
-        console.log('Config error: ' + err.message);
+    if (err) { log.error('Config: ' + err.message);
         process.exit(1);
     }
-});
 
-// load environment specific config options
-config.load('./config/env/' + env + '.json', function(err) {
-    if (err) {
-        console.log('Config warning: env "' + env + '", ' + err.message);
-        return;
-    }
+    // load environment specific config options
+    config.load('./config/env/' + env + '.json', function(err) {
+        if (err) {
+            log.warning('Config: env "' + env + '", ' + err.message);
+            return;
+        }
+
+        // set verbose level
+        if (config.verbose) log.level = 5;
+    });
 });
 
 module.exports = config;
