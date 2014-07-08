d87022976394f84734743ea4b1949fea337785d9
Alex Simenduev <shamil.si@gmail.com>
2014-07-08 10:14:36 +0300

    small changes/fixes + version bump to 0.3.4

--- config/index.js
+++ config/index.js
@@ -1,19 +1,23 @@
-// requires
-var fs          = require('fs'),
-    log         = require('sslog'),
-    config      = require('../lib/config');
+// get the NODE_ENV and 'appdir'
+var env     = process.env.NODE_ENV || 'development',
+    appdir  = require('path').resolve(__dirname, '..');
 
-// get the NODE_ENV
-var env = process.env.NODE_ENV;
+// requires
+var fs      = require('fs'),
+    log     = require('sslog'),
+    config  = require(appdir + '/lib/config');
+
+// allow to get appdir via config
+config.appdir = appdir;
 
 // load default config options
-config.load('./config/default.json', function(err) {
+config.load(appdir + '/config/default.json', function(err) {
     if (err) { log.error('Config: ' + err.message);
         process.exit(1);
     }
 
     // load environment specific config options
-    config.load('./config/env/' + env + '.json', function(err) {
+    config.load(appdir + '/config/env/' + env + '.json', function(err) {
         if (err) {
             log.warn('Config: env "' + env + '", ' + err.message);
             return;
@@ -31,16 +35,16 @@ config.load('./config/default.json', function(err) {
             {
                 if (config.ssl.ca)
                 {
-                    if (!Array.isArray(config.ssl.ca)) throw new ('config.ssl.ca: must be and Array of string(s)');
+                    if (!Array.isArray(config.ssl.ca)) throw new Error('config.ssl.ca: must be and Array of string(s)');
                     config.ssl.ca = config.ssl.ca.map(function(ca) { return fs.readFileSync(ca); });
                 }
                 if (config.ssl.pfx)  config.ssl.pfx = fs.readFileSync(config.ssl.pfx);
-                if (config.ssl.key)  config.ssl.key  = fs.readFileSync(config.ssl.key);                
-                if (config.ssl.cert) config.ssl.cert = fs.readFileSync(config.ssl.cert);                
+                if (config.ssl.key)  config.ssl.key  = fs.readFileSync(config.ssl.key);
+                if (config.ssl.cert) config.ssl.cert = fs.readFileSync(config.ssl.cert);
             }
-            catch (err)
+            catch (e)
             {
-                log.warn('SSL won\'t be enabled.' + err.message);
+                log.warn('SSL won\'t be enabled.' + e.message);
                 config.ssl.listen = false;
             }
         }
